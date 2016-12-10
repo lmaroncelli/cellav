d38d90a6d062d26d42f0e023b8936d844aa9b2ef
Luigi HP <lmaroncelli@gmail.com>
2016-12-10 10:02:28 +0100

    inizio grafica

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    
-    <!-- CSRF Token -->
+   <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="shortcut icon" href="assets/ico/favicon.ico">
@@ -19,21 +19,25 @@
         @yield('title')
       @endif
     </title>
-    
+
 
     <!-- Bootstrap core CSS -->
     <link href="{{ url('frontend/assets/css/bootstrap.css') }}" rel="stylesheet">
 
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="{{ url('frontend/assets/css/ie10-viewport-bug-workaround.css') }}" rel="stylesheet">
+
     <!-- Custom styles for this template -->
     <link href="{{ url('frontend/assets/css/style.css') }}" rel="stylesheet">
-    <link href="{{ url('frontend/assets/css/font-awesome.min.css') }}" rel="stylesheet">
+     <link href="{{ url('frontend/assets/css/font-awesome.min.css') }}" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
+  
     @yield('css')
     
     <!-- Scripts -->
@@ -42,109 +46,56 @@
             'csrfToken' => csrf_token(),
         ]); ?>
     </script>
+
+
   </head>
 
+  <body>
 
-<body>
-    <!-- Fixed navbar -->
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="{{ url('/') }}">HOME.</a>
+          <a class="navbar-brand" href="#">CELIACHIAMO</a>
         </div>
-        <div class="navbar-collapse collapse navbar-right">
+        <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            
-            {{-- Menu dinamico --}}
-            {!! Helper::menu() !!}
-            
-            {{-- Menu Utente Loggato oppure No --}}
-            @if (Auth::guest())
-                <li><a href="{{ url('/login') }}">Login</a></li>
-                <li><a href="{{ url('/register') }}">Register</a></li>
-            @else
-              
-              @if (Auth::user()->carrelli->count() > 0)
-                <li><a href="{{ route('carrello.show') }}">
-                    <i class="material-icons">carrello</i><span class="badge green">{{Auth::user()->carrelli()->first()->prodotti()->count()}}</span>  
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    </a>
-                </li>
-              @endif
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }} <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a href="{{ route('user.profile') }}">
-                      profile
-                    </a>
-                  </li>
-                  <li>
-                      <a href="{{ url('/logout') }}"
-                          onclick="event.preventDefault();
-                                   document.getElementById('logout-form').submit();">
-                          Logout
-                      </a>
-
-                      <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                          {{ csrf_field() }}
-                      </form>
-                  </li>
-                </ul>
-              </li>
-            @endif
-            {{-- FINE Menu utente --}}
-
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
-    </div>
-    
-    <!-- *****************************************************************************************************************
-     BLUE WRAP
-     ***************************************************************************************************************** -->
-    <div id="blue">
-        <div class="container">
-            <div class="row">
-                <h3>@yield('title').</h3>
-            </div><!-- /row -->
-        </div> <!-- /container -->
-    </div><!-- /blue -->
+    </nav>
 
-     
-    <!-- *****************************************************************************************************************
-     AGENCY ABOUT
-     ***************************************************************************************************************** -->
+    <div class="container">
 
-     <div class="container mtb">
-        <div class="row">
-            <div class="col-lg-10 col-lg-offset-2">
-                 @include('admin.show_errors')
-                 @yield('content')
-            </div>
-        </div><! --/row -->
-     </div><! --/container -->
-    
-      <!-- Bootstrap core JavaScript
-      ================================================== -->
-      <!-- Placed at the end of the document so the pages load faster -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-      <script src={{ url('frontend/assets/js/bootstrap.min.js') }}></script>
-    <script src="{{ url('frontend/assets/js/retina-1.1.0.js') }}"></script>
-    <script src="{{ url('frontend/assets/js/jquery.hoverdir.js') }}"></script>
-    <script src="{{ url('frontend/assets/js/jquery.hoverex.min.js') }}"></script>
-    <script src="{{ url('frontend/assets/js/jquery.prettyPhoto.js') }}"></script>
-      <script src="{{ url('frontend/assets/js/jquery.isotope.min.js') }}"></script>
-      <script src="{{ url('frontend/assets/js/custom.js') }}"></script>
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
 
+        @include('admin.show_errors')
+        @yield('content')
+
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="{{ url('frontend/assets/js/bootstrap.min.js') }}"><\/script>')</script>
+    <script src={{ url('frontend/assets/js/bootstrap.min.js') }}></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src={{ url('frontend/assets/js/ie10-viewport-bug-workaround.js') }}></script>
 
     @yield('script')
-    
-</body>
+  </body>
 </html>
