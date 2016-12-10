4bd25b1db706e039295fa68cedd5115988dcd260
Luigi HP <lmaroncelli@gmail.com>
2016-12-10 13:09:35 +0100

    img negozi

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -112,7 +112,11 @@
     <div class="container">
 
       <div class="starter-template">
-        @include('slider')
+        
+        @include('home.slider')
+
+        @include('home.negozi')
+
         <h1>Bootstrap starter template</h1>
         <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
 
