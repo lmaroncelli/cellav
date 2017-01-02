29ef475459687beda58787f7c026beaf157e48ee
vagrant <vagrant@homestead>
2017-01-02 11:58:20 +0000

    google map hp migration

---
+++ database/migrations/2017_01_02_115114_add_goggle_map_to_homepage.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGoggleMapToHomepage extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tblHomePages', function (Blueprint $table) {
+            $table->string('gm_nome')->default('')->after('desc_tiburtina');
+            $table->string('gm_indirizzo')->default('')->after('gm_nome');
+            $table->string('gm_lat',10)->default('')->after('gm_indirizzo');
+            $table->string('gm_long',10)->default('')->after('gm_lat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tblHomePages', function (Blueprint $table) {
+            $table->dropColumn('gm_nome');
+            $table->dropColumn('gm_indirizzo');
+            $table->dropColumn('gm_lat');
+            $table->dropColumn('gm_long');
+        });
+    }
+}
